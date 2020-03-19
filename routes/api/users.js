5b58b0f6f21327aa673eb08aec210ec1e325a20d
Alexander Masud <alexandermasud@gmail.com>
2020-03-19 08:47:43 +0100

    Implementing jsonwebtoken

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const gravatar = require("gravatar");
 const { check, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 const User = require("../../models/User");
 
@@ -37,7 +39,9 @@ router.post(
         email: email
       });
       if (user) {
-        res.status(400).json({ errors: [{ msg: "Email används redan" }] });
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Email används redan" }] });
       }
 
       // Get users gravatar
@@ -64,7 +68,21 @@ router.post(
       await user.save();
 
       // Return jsonwebtoken
-      res.send("User registered");
+      const payload = {
+        user: {
+          id: user._id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server error");
