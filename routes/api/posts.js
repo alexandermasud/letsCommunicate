fbf1470291828ca9e04793ea37c4f127eda700e5
Alexander Masud <alexandermasud@gmail.com>
2020-03-20 11:12:44 +0100

    Get posts and delete post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -44,4 +44,70 @@ router.post(
   }
 );
 
+// @route    GET api/posts
+// @desc     Get all posts
+// @access   Private
+
+router.get("/", auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({
+      date: -1
+    });
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route    GET api/posts/:id
+// @desc     Get post by id
+// @access   Private
+
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      res.status(404).json({ msg: "Hittade inget inlägg" });
+    }
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === "ObjectId") {
+      res.status(404).json({ msg: "Hittade inget inlägg" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route    DELETE api/posts/:id
+// @desc     Delete a post by id
+// @access   Private
+
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      res.status(404).json({ msg: "Hittade inget inlägg" });
+    }
+
+    // Check if correct user trying to delete post
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "Användare ej autentiserad" });
+    }
+
+    await post.remove();
+
+    res.json({ msg: "Inlägg borttaget" });
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === "ObjectId") {
+      res.status(404).json({ msg: "Hittade inget inlägg" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
