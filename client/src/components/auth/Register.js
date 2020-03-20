ca286a4b8a2b19bba1f23d0366abebc5f2316d67
Alexander Masud <alexandermasud@gmail.com>
2020-03-20 18:33:26 +0100

    Registration form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,88 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
 
 const Register = () => {
-  return <div>Register</div>;
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: ""
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = e => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    if (password !== password2) {
+      console.log("Lösenord matchar inte");
+    } else {
+      console.log(formData);
+    }
+  };
+
+  return (
+    <Fragment>
+      <h1 className="large text-primary">Registrera</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Skapa konto
+      </p>
+      <form className="form" onSubmit={e => onSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Namn"
+            name="name"
+            value={name}
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            value={email}
+            onChange={e => onChange(e)}
+            required
+          />
+          <small className="form-text">
+            Denna site använder
+            <a href="https://en.gravatar.com"> Gravatar </a>för profilbilder
+          </small>
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Lösenord"
+            name="password"
+            value={password}
+            minLength="6"
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Upprepa lösenord"
+            name="password2"
+            value={password2}
+            minLength="6"
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Registrera" />
+      </form>
+      <p className="my-1">
+        Har du redan ett konto? <a href="login.html">Logga in</a>
+      </p>
+    </Fragment>
+  );
 };
 
 export default Register;
