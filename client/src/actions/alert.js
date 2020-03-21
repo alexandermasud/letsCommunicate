a8f8e142a1d27d5cd91c0a958b080b7ab232c6b5
Alexander Masud <alexandermasud@gmail.com>
2020-03-21 10:35:05 +0100

    Alert component

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,7 +1,8 @@
 import { SET_ALERT, REMOVE_ALERT } from "./types";
+
 import { v4 as uuid } from "uuid";
 
-export const setAlert = (msg, alertType) => dispatch => {
+export const setAlert = (msg, alertType, timeout = 5000) => dispatch => {
   const id = uuid();
   dispatch({
     type: SET_ALERT,
@@ -11,4 +12,13 @@ export const setAlert = (msg, alertType) => dispatch => {
       id
     }
   });
+
+  setTimeout(
+    () =>
+      dispatch({
+        type: REMOVE_ALERT,
+        payload: id
+      }),
+    timeout
+  );
 };
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,10 @@
 import React, { Fragment, useState } from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import { setAlert } from "../../actions/alert";
+import PropTypes from "prop-types";
 
-const Register = () => {
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -18,7 +21,7 @@ const Register = () => {
   const onSubmit = async e => {
     e.preventDefault();
     if (password !== password2) {
-      console.log("Lösenord matchar inte");
+      setAlert("Lösenorden matchar inte", "danger");
     } else {
       console.log("Success");
     }
@@ -86,4 +89,8 @@ const Register = () => {
   );
 };
 
-export default Register;
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired
+};
+
+export default connect(null, { setAlert })(Register);
